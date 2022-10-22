5e5658da7ce657979a13fdf87d0925965bfdc8a3
Mochenghualei <18830279823@163.com>
2022-10-22 21:58:08 +0800

    完善桶排序

--- src/views/Num1.vue
+++ src/views/Num1.vue
@@ -1,5 +1,5 @@
 <template>
-  <h3>简单桶排序(1~100,最多100个)，输入数字，以","隔开，点击按钮查看排序结果</h3>
+  <h3>简单桶排序输入整数，以","隔开，点击按钮查看排序结果</h3>
   <input type="text" v-focus ref="input" v-model.trim="sortNum">
   <button @click="handleClick('asc')">升序排序</button>
   <button @click="handleClick('desc')">降序排序</button>
@@ -19,15 +19,11 @@ const res = ref([])
 const sortNum = ref("")
 function handleClick(sortBy) {
   const sortArr = sortNum.value.replace(/\s*/g, "").split(",").map((num) => Number(num))
-  if (sortArr.some((num) => num > 100 || num < 1)) {
-    alert("请输入1~100以内的数字！")
-    input.value.focus()
-  } else if (sortArr.length >= 100) {
-    alert("最多输入100个数字！")
-    input.value.focus()
-  } else {
-    bucketSort(sortArr, sortBy)
-  }
+
+  const intNum = Math.max(...sortArr)
+
+  bucketSort(sortArr, intNum, sortBy)
+
   setTimeout(() => {
     sortNum.value = ""
   }, 100)
@@ -35,11 +31,11 @@ function handleClick(sortBy) {
 
 // 核心逻辑
 // [5, 3, 5, 2, 8]
-function bucketSort(sortArr, sortBy) {
+function bucketSort(sortArr, intNum, sortBy) {
   let resArr = []
-  // 0~100,创建101个桶，初始化为0
-  const arr = new Array(101)
-  for (let i = 0; i <= 100; i++) {
+  // 创建intNUm+1个桶
+  const arr = new Array(intNum + 1)
+  for (let i = 0; i <= intNum; i++) {
     arr[i] = 0
   }
 
@@ -51,14 +47,14 @@ function bucketSort(sortArr, sortBy) {
   // 依次循环遍历打印目标
   if (sortBy == 'asc') {
     // 升序
-    for (let i = 0; i <= 100; i++) {
+    for (let i = 0; i <= intNum; i++) {
       for (let j = 1; j <= arr[i]; j++) {
         resArr.push(i)
       }
     }
   } else {
     // 降序
-    for (let i = 10; i >= 0; i--) {
+    for (let i = intNum; i >= 0; i--) {
       for (let j = 0; j < arr[i]; j++) {
         resArr.push(i)
       }
